4d489c9bd2cd0eef65c4d1eebab3f67afe8a8367
Vivek Limbachiya <vivek.limbachiya@pardypanda.com>
2024-09-03 15:10:35 +0530

    map

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedin />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/Vivek24051" },
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/vivek-limbachiya-a33a26242?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
   { icon: <FaYoutube />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaTwitter />, path: "https://x.com/VivekL24?t=fM-ZNTXk-BKPIljP6AJc6A&s=09" },
 ];
 
 const Social = ({containerStyles, iconStyles}) => {
